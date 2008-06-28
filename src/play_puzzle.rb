bb90f54a9f58d20d7a040d4c7604b89352e79954
Pierre-Henri Trivier <phtrivier@wilde.(none)>
2008-06-28 16:49:11 +0200

    * lib for gosu * added display of cells (ugly for the moment)

---
+++ src/play_puzzle.rb
@@ -0,0 +1,103 @@
+begin
+  # In case you use Gosu via RubyGems.
+  require 'rubygems'
+rescue LoadError
+  # In case you don't.
+end
+
+require 'gosu'
+
+module ZOrder
+  Background, Stars, Player, UI = *0..3
+end
+
+require 'puzzle'
+
+class SimplePuzzle < Puzzle
+  dim 3,3
+  row "###"
+  row "I##"
+  row "--O"
+end
+
+class GameWindow < Gosu::Window
+  def initialize
+    super(640, 480, false)
+    self.caption = "Puzzle Game"
+
+    @puzzle = SimplePuzzle.new
+
+#    @background_image = Gosu::Image.new(self, "media/Space.png", true)
+
+#    @player = Player.new(self)
+#    @player.warp(320, 240)
+
+#    @star_anim = Gosu::Image::load_tiles(self, "media/Star.png", 25, 25, false)
+#    @stars = Array.new
+
+    @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+
+
+    @colors = { Wall => Gosu::Color.new(0xff00ff00),
+      Walkable => Gosu::Color.new(0xffff0000),
+      In => Gosu::Color.new(0xff000000),
+      Out => Gosu::Color.new(0xff0000ff) }
+
+  end
+
+  def update
+  end
+
+  def draw
+    @font.draw("Hello world !", 10, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
+    draw_puzzle
+  end
+
+  def draw_puzzle
+
+    x0 = 20
+    y0 = 50
+    s = 30
+
+    @puzzle.each_cell do |i,j,c|
+
+      x = x0 + j * s
+      y = y0 + i * s
+
+      # TODO : GET THE COLOR
+      #color = get_color(c.color_name)
+      color = get_color(c)
+
+      # DRAW A SQUARE OF THE COLOR, OF THE PROPER SIZE, ETC...
+
+#      drawQuad (double x1, double y1, Color c1, double x2, double y2, Color c2, double x3, double y3, Color c3, double x4, double y4, Color c4, ZPos z, AlphaMode mode=amDefault)
+
+      puts "Drawing cell : #{i},#{j}, #{c}"
+
+      draw_quad( x, y, color,
+                  x + s, y , color,
+                  x, y +s, color,
+                  x +s, y+s, color,
+                  ZOrder::UI)
+
+    end
+
+  end
+
+  def get_color(cell)
+    # make it use image instead of colors
+    res = @colors[cell.class]
+    puts "Using color #{res}, for class #{cell.class}"
+    res
+  end
+
+  def button_down(id)
+    if id == Gosu::Button::KbEscape then
+      close
+    end
+  end
+
+end
+
+w = GameWindow.new
+w.show
