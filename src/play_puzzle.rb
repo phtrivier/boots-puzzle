f59d3f69c0cca4d9ea67a514927745f6a1ec82f1
Pierre-Henri Trivier <phtrivier@wilde.(none)>
2008-07-06 23:28:15 +0200

    GUI know moves the player !!

--- src/play_puzzle.rb
+++ src/play_puzzle.rb
@@ -26,6 +26,7 @@ class GameWindow < Gosu::Window
     self.caption = "Puzzle Game"
 
     @puzzle = SimplePuzzle.new
+    @puzzle.enters_player!
 
 #    @background_image = Gosu::Image.new(self, "media/Space.png", true)
 
@@ -46,6 +47,20 @@ class GameWindow < Gosu::Window
   end
 
   def update
+
+    if button_down? Gosu::Button::KbLeft or button_down? Gosu::Button::GpLeft then
+      @puzzle.try_move!(:left)
+    end
+    if button_down? Gosu::Button::KbUp or button_down? Gosu::Button::GpUp then
+      @puzzle.try_move!(:up)
+    end
+    if button_down? Gosu::Button::KbDown or button_down? Gosu::Button::GpDown then
+      @puzzle.try_move!(:down)
+    end
+    if button_down? Gosu::Button::KbRight or button_down? Gosu::Button::GpRight then
+      @puzzle.try_move!(:right)
+    end
+
   end
 
   def draw
@@ -55,39 +70,62 @@ class GameWindow < Gosu::Window
 
   def draw_puzzle
 
-    x0 = 20
-    y0 = 50
-    s = 30
+    @x0 = 20
+    @y0 = 50
+    @s = 30
 
     @puzzle.each_cell do |i,j,c|
+      draw_cell(i,j,c)
+    end
 
-      x = x0 + j * s
-      y = y0 + i * s
+    draw_player
+
+  end
+
+  def draw_player
+    i,j = @puzzle.player.pos
+
+    ps = @s / 3
+    delta_ps = @s / 3
+
+    x = @x0 + j * @s + delta_ps
+    y = @y0 + i * @s + delta_ps
+
+    color = Gosu::Color.new(0xff00ffff)
+
+    draw_quad( x , y , color,
+                  x + ps, y, color,
+                  x, y + ps, color,
+                  x + ps, y+ ps, color,
+                  ZOrder::UI)
+
+  end
+
+  def draw_cell(i,j,c)
+
+      x = @x0 + j * @s
+      y = @y0 + i * @s
 
       # TODO : GET THE COLOR
       #color = get_color(c.color_name)
       color = get_color(c)
 
       # DRAW A SQUARE OF THE COLOR, OF THE PROPER SIZE, ETC...
-
 #      drawQuad (double x1, double y1, Color c1, double x2, double y2, Color c2, double x3, double y3, Color c3, double x4, double y4, Color c4, ZPos z, AlphaMode mode=amDefault)
-
-      puts "Drawing cell : #{i},#{j}, #{c}"
+      #puts "Drawing cell : #{i},#{j}, #{c}"
 
       draw_quad( x, y, color,
-                  x + s, y , color,
-                  x, y +s, color,
-                  x +s, y+s, color,
+                  x + @s, y , color,
+                  x, y + @s, color,
+                  x + @s, y+ @s, color,
                   ZOrder::UI)
-
-    end
 
   end
 
   def get_color(cell)
     # make it use image instead of colors
     res = @colors[cell.class]
-    puts "Using color #{res}, for class #{cell.class}"
+    # puts "Using color #{res}, for class #{cell.class}"
     res
   end
 
