e3208189f3bc69cb5af787d6075c8e0853e23080
Pierre-Henri Trivier <phtrivier@wilde.(none)>
2008-07-20 20:27:50 +0200

    Jungling with boots !
    You can pick , drop, put on , an put off shoes

--- src/player.rb
+++ src/player.rb
@@ -6,6 +6,19 @@ class Player
 
   def initialize
     @current_boots = BareFeet.new
+    @boots = { :right => nil, :left => nil }
+  end
+
+  def boots_in_right_hand
+    @boots[:right]
+  end
+
+  def boots_in_left_hand
+    @boots[:left]
+  end
+
+  def free_hand?
+    @boots[:right] == nil || @boots[:left] == nil
   end
 
   def pos
@@ -16,5 +29,63 @@ class Player
     @i,@j = pos
   end
 
+  def pick!(boots)
+    if (!free_hand?)
+      raise RuntimeError.new("No free hand to pick boots")
+    end
+    if (@boots[:right] == nil)
+      @boots[:right] = boots
+    elsif (@boots[:left] == nil)
+      @boots[:left] = boots
+    end
+  end
 
+  # TODO : boots should be droped somewhere ;)
+  def drop!(side)
+    if (side == :right)
+      @boots[:right] = nil
+    elsif (side == :left)
+      @boots[:left] = nil
+    else
+      raise RuntimeError.new("Wrong side to drop : #{side}")
+    end
+  end
+
+  def put_on!(side)
+    if (bare_feet?)
+      @current_boots = @boots[side]
+      @boots[side] = nil
+    else
+      swap_current_with_hand(side)
+    end
+  end
+
+  def swap_current_with_hand(side)
+      tmp = @current_boots
+      @current_boots = @boots[side]
+      @boots[side] = tmp
+  end
+
+  # Put the current boots off, and pick
+  # them in any free hand.
+  # If no hand is free, the current pair of boots
+  # is swapped with the on in right hand.
+  def put_off!
+    if (@boots[:right] == nil)
+      @boots[:right] = @current_boots
+      @current_boots = BareFeet.new
+    elsif (@boots[:left] == nil)
+      @boots[:left] = @current_boots
+      @current_boots = BareFeet.new
+    else
+      # By default, exchange with the right side
+      swap_current_with_hand(:right)
+    end
+
+  end
+
+  # Is the player walking on bare feet ?
+  def bare_feet?
+    @current_boots.class == BareFeet
+  end
 end
--- src/player_test.rb
+++ src/player_test.rb
@@ -4,9 +4,102 @@ require 'test/unit'
 
 class PlayerTest < Test::Unit::TestCase
 
+  def setup
+    @p = Player.new
+  end
+
   def test_player_has_bare_feer_by_default
-    p = Player.new
-    assert_equal(BareFeet, p.current_boots.class)
+    assert_equal(BareFeet, @p.current_boots.class)
+  end
+
+  def test_player_has_no_boots_in_hand_by_default
+    assert_nil(@p.boots_in_right_hand)
+    assert_nil(@p.boots_in_left_hand)
+    assert(@p.free_hand?)
+  end
+
+  def test_player_can_pick_boots
+    b1 = Boots.new
+    @p.pick!(b1)
+    assert_equal(b1, @p.boots_in_right_hand)
+    assert(@p.free_hand?)
+    b2 = Boots.new
+    @p.pick!(b2)
+    assert_equal(b2, @p.boots_in_left_hand)
+    assert(!@p.free_hand?)
+    b3 = Boots.new
+    begin
+      @p.pick!(b3)
+      assert(false, "Should not be possible to pick shoes with full hands")
+    rescue RuntimeError => e
+      assert_equal("No free hand to pick boots", e.message)
+    end
+  end
+
+  def test_player_can_drop_boots
+    b1 = Boots.new
+    @p.pick!(b1)
+    assert_equal(b1, @p.boots_in_right_hand)
+    assert(@p.free_hand?)
+    b2 = Boots.new
+    @p.pick!(b2)
+    assert_equal(b2, @p.boots_in_left_hand)
+    assert(!@p.free_hand?)
+
+    @p.drop!(:right)
+    assert_nil(@p.boots_in_right_hand)
+    b3 = Boots.new
+    @p.pick!(b3)
+    assert_equal(b3, @p.boots_in_right_hand)
+
+    begin
+      @p.drop!(:foo)
+      assert(false, "Foo is not a valid side")
+    rescue RuntimeError => e
+      assert_equal("Wrong side to drop : foo", e.message)
+    end
+  end
+
+  def test_player_can_put_boots_on_and_off
+    # One pair of boots in hand
+    b1 = Boots.new
+    @p.pick!(b1)
+    @p.put_on!(:right)
+    assert_equal(b1, @p.current_boots)
+
+    @p.put_off!
+    assert_equal(BareFeet, @p.current_boots.class)
+    assert_equal(b1, @p.boots_in_right_hand)
+
+    # A second pair of boots in hand
+    b2 = Boots.new
+    @p.pick!(b2)
+    @p.put_on!(:left)
+    assert_equal(b2, @p.current_boots)
+
+    @p.put_off!
+    assert(@p.bare_feet?)
+    assert_equal(b2, @p.boots_in_left_hand)
+
+    # Now put on one of the pairs, and pick a
+    # third one
+    @p.put_on!(:right)
+    b3 = Boots.new
+    @p.pick!(b3)
+    assert_equal(b1, @p.current_boots)
+    assert_equal(b2, @p.boots_in_left_hand)
+    assert_equal(b3, @p.boots_in_right_hand)
+    # Put in on ; worn shoes goes in the free hand
+    @p.put_on!(:right)
+    assert_equal(b3, @p.current_boots)
+    assert_equal(b2, @p.boots_in_left_hand)
+    assert_equal(b1, @p.boots_in_right_hand)
+    # Now put the current boots off : where does it go ?
+    # By default, I exchange it with the right side
+    @p.put_off!
+    assert_equal(b1, @p.current_boots)
+    assert_equal(b2, @p.boots_in_left_hand)
+    assert_equal(b3, @p.boots_in_right_hand)
   end
 
 end
