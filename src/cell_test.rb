0d29712c4074223e87c2f0cad9db635a5b9f3385
Pierre-Henri Trivier <phtrivier@wilde.(none)>
2008-08-01 00:06:31 +0200

    Added GPL  banners

--- src/cell_test.rb
+++ src/cell_test.rb
@@ -1,3 +1,24 @@
+# Boots Puzzle - cell_test.rb
+#
+# Tests for the Cell class
+#
+# Copyright (C) 2008 Pierre-Henri Trivier
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+
+
 require 'test/unit'
 
 require 'cell'
