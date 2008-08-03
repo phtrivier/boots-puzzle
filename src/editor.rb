21e928abd30e97bef22df059085a420513361e4a
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2008-08-03 17:06:51 +0200

    Basic tooling support in the editor (can be *much* improved)

--- src/editor.rb
+++ src/editor.rb
@@ -20,9 +20,11 @@
 
 require 'puzzle'
 
-
 class Editor < Shoes
   url '/', :index
+
+  LEFT_BUTTON = 1
+  RIGHT_BUTTON = 3
 
   def cell(i, j, t)
     stack :width => '40px' do
@@ -38,14 +40,20 @@ class Editor < Shoes
         # Then change all the zones as required !!
         # TODO : ADD THE HOVER TO CHANGE THE BACKGROUND COLOR IF REQUIRED
 
-        puts "Clicked on #{i}, #{j} ... original type was #{t}"
+        debug "Clicked on #{i}, #{j} ... original type was #{t}"
 
+        if (b == LEFT_BUTTON)
+          @new_type = @left_tool_type
+        elsif (b == RIGHT_BUTTON)
+          @new_type = @right_tool_type
+        end
+        debug "New type to be applied : #{@new_type}"
         @cells[i][j] = @new_type
-        puts img.path
-        img.remove
-        img = image @new_type.new.src
 
-        puts img.path
+        img.path = @new_type.new.src
+        debug "New image path : #{img.path}"
+        img.hide
+        img.show
 
       end
 
@@ -54,7 +62,7 @@ class Editor < Shoes
 
   def index
 
-     # FIXME : use unhardwired values
+    # FIXME : use unhardwired values
     @w = 10;
     @h = 7;
 
@@ -62,27 +70,92 @@ class Editor < Shoes
 
     @new_type = Wall
 
+    @left_tool_img =  nil
+    @left_tool_type = nil
+    @right_tool_img = nil
+    @right_tool_type = nil
+
     show_editor
+  end
+
+  def cell_button(klass)
+
+    stack :width => "50%" do
+      image klass.new.src
+
+      click do |b,l,t|
+
+        debug "Value of b : #{b}"
+        debug "Is it left ? #{b == LEFT_BUTTON}. Is it 1 ? #{b == 1}.}"
+        debug "Is it right ? #{b == RIGHT_BUTTON}. Is it 3 ? #{b == 3}.}"
+
+        if (b == LEFT_BUTTON)
+          debug "left, new path : #{klass.new.src}"
+          @left_tool_img.path = klass.new.src
+          @left_tool_img.hide
+          @left_tool_img.show
+          @left_tool_type = klass
+        elsif (b == RIGHT_BUTTON)
+          debug "right, new path : #{klass.new.src}"
+          @right_tool_img.path = klass.new.src
+          @right_tool_img.hide
+          @right_tool_img.show
+          @right_tool_type = klass
+        end
+
+      end
+
+    end
+
   end
 
   def show_editor
     flow do
       stack :width => '20%' do
         border black, :strokewidth => 1
-        para "list of available tools"
-        para "list of selected tools"
+
+        para "Available tools"
+
+        stack do
+          border black, :strokewidth => 1
+          # TODO : Make this more generic by
+          # Loading all the sub-classes of Cell
+          # and cutting them in three (quite easy, actually !!)
+          flow :margin_top => '5px', :margin_left => '5px' do
+            cell_button(In)
+            cell_button(Out)
+          end
+
+          flow :margin_top => '5px', :margin_left => '5px' do
+            cell_button(Wall)
+            cell_button(Walkable)
+          end
+
+        end
+
+        para "Selected tools"
+        stack do
+          border black, :strokewidth => 1
+          para "Left"
+          @left_tool_type = Wall
+          @left_tool_img = image @left_tool_type.new.src
+          para "Right"
+          @right_tool_type = Walkable
+          @right_tool_img = image @right_tool_type.new.src
+        end
+
       end
 
       stack :width => '60%' do
         border black, :strokewidth => 1
 
         (0..@h).each do |i|
-          # puts "adding a row ? "
+          # debugs "adding a row ? "
           @cells[i] = []
           flow :margin => 5 do
             @w.times do |j|
 
-              # puts "adding a cell ?"
+              # debugs "adding a cell ?"
 
               cell(i, j , Walkable)
 
